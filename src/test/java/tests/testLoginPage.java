90402d748e3f2ddf26ddf7624f06e0e1864b0b41
Mahmudul Hasan <mahmudol.tushar@gmail.com>
2025-05-03 10:00:05 +0600

    update: 2025-04-27 21:26:13.573000000 +0000

--- src/test/java/tests/testLoginPage.java
+++ src/test/java/tests/testLoginPage.java
@@ -16,12 +16,9 @@ public class TestLoginPage extends BaseTest{
 
     @Parameters({"username","password"})
     @Test
-    public void loginWithValidCred(String username, String password) {
-        //String username = prop.getProperty("userName");
-        //String password = prop.getProperty("password");
-        //System.out.println(username + " " +
-    //    String username =prop.getProperty("userName");
-    //    String password =prop.getProperty("password");
+    public void loginWithValidCred(String username,String password){
+//        String userName =prop.getProperty("userName");
+//        String password =prop.getProperty("password");
         String title = loginPage.getTitle();
         loginPage.setUserName(username);
         loginPage.setPassword(password);
